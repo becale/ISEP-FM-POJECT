d69fb2e5f499f0bbb67d9159a3fc79625a7dfbde
Futler <bricetatchou68@google.com>
2023-10-25 15:31:22 +0100

    POSTE ISEP OK

--- ISEP-FM-PROJECT/ISEP_FM_PROJECT/settings.py
+++ ISEP-FM-PROJECT/ISEP_FM_PROJECT/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
+
     'bulletin.apps.BulletinConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +53,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+  'http://localhost:8000',
+
+  "http://127.0.0.1:8000",
+)
 
 ROOT_URLCONF = 'ISEP_FM_PROJECT.urls'
 
